2ce1b0dc46a98331ec2d52e4b9c5a3082f6c1a0c
Lily Bhattacharjee <lbhattacharjee@berkeley.edu>
2021-11-27 18:18:27 -0800

    Implemented basic frontend -- parsing example csv log file (not connected to backend) + log

--- frontend/index.js
+++ frontend/index.js
@@ -1,3 +1,174 @@
-d3.csv("frontend_log.csv", function(data) {
-    console.log(data);
+d3.csv("frontend_log.csv").then(function(data) {
+    displayLog(data);
+    generateForceGraph(data);
 });
+
+
+// create the force directed graph
+function generateGraphData(data) {
+    let names = [];
+    for (const elem of data) {
+        let sender = elem["Sender"];
+        let recipient = elem["Recipient"];
+
+        names.push(sender);
+        names.push(recipient);
+    }
+
+    let nodes = [];
+    let unique_names = [... new Set(names)];
+    let name_id_mapper = {};
+    let id = 0;
+    for (const i of unique_names) {
+        nodes.push({
+            "id": id,
+            "name": i,
+        })
+        name_id_mapper[i] = id;
+        id++;
+    }
+
+    let links = [];
+    for (const elem of data) {
+        let sender = name_id_mapper[elem["Sender"]];
+        let recipient = name_id_mapper[elem["Recipient"]];
+
+        links.push({
+            "source": sender,
+            "target": recipient,
+            "value": 1,
+        })
+    }
+
+    let graphData = {
+        "nodes": nodes,
+        "links": links,
+    }
+
+    return graphData;
+}
+
+function displayLog(data) {
+    let log = document.getElementById("log");
+    let displayHTML = `<h5>Log</h5>
+        <table id = 'logTable' style = 'width: 100%;'>
+        <tr>
+            <th>Sender</th>
+            <th>Recipient</th>
+            <th>Phase</th>
+        </tr>`;
+    for (const logEntry of data) {
+        let currDisplay = "<tr><td>" + logEntry["Sender"] + "</td><td>" + logEntry["Recipient"] + "</td><td>" + logEntry["Type"] + "</td></tr>";
+        displayHTML += currDisplay;
+    }
+    displayHTML += "</table>";
+    log.innerHTML = displayHTML;
+}
+
+function generateForceGraph(data) {
+    let width = 640;
+    let height = 400;
+    let idx = 0;
+
+    let graphData = generateGraphData(data);
+    let nodes = graphData["nodes"];
+    let sequentialLinks = graphData["links"]
+    let visibleLinks = [sequentialLinks[idx]];
+
+    const forceNode = d3.forceManyBody();
+    const forceLink = d3.forceLink(visibleLinks).strength(0);
+
+    const simulation = d3.forceSimulation(nodes)
+        .force("link", forceLink)
+        .force("charge", forceNode)
+        .force("center", d3.forceCenter())
+        .on("tick", ticked);
+
+    let svg = d3.select("#visualization")
+        .append("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .attr("viewBox", [-width / 2, -height / 2, width, height])
+        .attr("style", "max-width: 100%; height: auto; height: intrinsic;")
+
+    let link = svg.append("g")
+        .attr("stroke", "black")
+        .attr("stroke-opacity", 0.6)
+        .attr("stroke-width", 5)
+    .selectAll("path")
+    .data(visibleLinks)
+    .join("path")
+    .attr("marker-end","url(#end-arrow)");
+
+    svg.append("svg:defs").append("svg:marker")
+        .attr("id", "end-arrow")
+        .attr("viewBox", "0 -5 10 10")
+        .attr("refX", 10)
+        .attr("markerWidth", 3)
+        .attr("markerHeight", 3)
+        .attr("orient", "auto")
+        .append("svg:path")
+        .attr("d", "M0,-5L10,0L0,5")
+        .attr("fill", "black");
+
+    let node = svg.append("g")
+        .attr("fill", "red")
+        .attr("stroke", "black")
+    .selectAll("circle")
+    .data(nodes)
+    .join("circle")
+        .attr("r", 5);
+
+    let text = svg.selectAll("text")
+        .data(nodes)
+        .enter()
+        .append("text")
+
+    let textLabels = text
+        .attr("x", d => d.x + 10)
+        .attr("y", d => d.y + 10)
+        .text(d => d.name)
+
+    linkArc = d =>`M${d.source.x},${d.source.y}A0,0 0 0,1 ${d.target.x},${d.target.y}`
+
+    let log = document.getElementById("logTable");
+    let logRows = log.querySelectorAll("tr");
+    let currRow = logRows[idx + 1];
+    currRow.style["font-weight"] = "bold";
+
+    function ticked() {
+        link.attr("d", linkArc);
+
+        node 
+            .attr("cx", d => d.x)
+            .attr("cy", d => d.y);
+
+        textLabels
+            .attr("dx", d => d.x + 10)
+            .attr("dy", d => d.y + 10);
+    }
+
+    function update() {
+        idx++;
+        if (idx >= sequentialLinks.length) {
+            return;
+        }
+
+        visibleLinks = [sequentialLinks[idx]];
+
+        link = link
+            .data(visibleLinks)
+            .join("line");
+
+        simulation.force("link").links(visibleLinks);
+        simulation.restart();
+
+        currRow.style["font-weight"] = "";
+        currRow = logRows[idx + 1];
+        currRow.style["font-weight"] = "bold";
+    }
+
+    const next = d3.select("#nextButton")
+        .on("click", update);
+}
+ 
